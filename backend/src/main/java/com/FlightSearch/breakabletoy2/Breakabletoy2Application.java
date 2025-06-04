0bd9ed470802cd6de3e92e694b956231c85877e4
SantiagoLuna109 <santiago.luna@encora.com>
2025-06-04 14:47:04 -0400

    Initilize the githb repository

---
+++ backend/src/main/java/com/FlightSearch/breakabletoy2/Breakabletoy2Application.java
@@ -0,0 +1,13 @@
+package com.FlightSearch.breakabletoy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Breakabletoy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Breakabletoy2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/FlightSearch/breakabletoy2/Breakabletoy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlightSearch.breakabletoy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Breakabletoy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
