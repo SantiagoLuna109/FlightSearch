ea6e1a54c32a60ce3891d8160b5631656496b168
SantiagoLuna109 <santiago.luna@encora.com>
2025-06-06 11:36:00 -0400

    Uodate using the Configuration Properties

--- backend/src/main/java/com/FlightSearch/breakabletoy2/Breakabletoy2Application.java
+++ backend/src/main/java/com/FlightSearch/breakabletoy2/Breakabletoy2Application.java
@@ -2,8 +2,10 @@ package com.FlightSearch.breakabletoy2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Breakabletoy2Application {
 
 	public static void main(String[] args) {
